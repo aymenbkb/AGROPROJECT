cc71aa650c88c702692218d93260313f55d0e1ba
AYMYAC <>
2024-06-23 05:06:17 +0100

    AGROPROJECT
    update

--- configfirebase.js
+++ configfirebase.js
@@ -6,14 +6,15 @@ import { getStorage } from "./firebase-storage.js";
 import { getDatabase } from "./firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtR9e2E5rPzTw6KH18JbFZWI44nfbFeZk",
-    authDomain: "agroboumediene.firebaseapp.com",
-    projectId: "agroboumediene",
-    storageBucket: "agroboumediene.appspot.com",
-    messagingSenderId: "327726045113",
-    appId: "1:327726045113:web:a988ee175852a46d730868",
-    measurementId: "G-T3H0541NDK"
-};
+    apiKey: "AIzaSyBj8molb1_4DDGhwQyfdKeR4HH7jJjPWjM",
+    authDomain: "prosemagro.firebaseapp.com",
+    databaseURL: "https://prosemagro-default-rtdb.firebaseio.com",
+    projectId: "prosemagro",
+    storageBucket: "prosemagro.appspot.com",
+    messagingSenderId: "210148411864",
+    appId: "1:210148411864:web:898f06e0863ad28e1d0aad",
+    measurementId: "G-050MB87EC4"
+  };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
